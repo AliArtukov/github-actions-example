2a4b46b8a3e15d9d9a45ddcbada97001e1ea7370
ortukov.s <ali.artukov@gmail.com>
2023-03-26 15:44:09 +0500

    added text for check auto ci/cd and running container

--- src/main/java/uz/atoy/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/uz/atoy/githubcicdactions/GithubCicdActionsApplication.java
@@ -11,11 +11,3 @@ public class GithubCicdActionsApplication {
     }
 
 }
-
-//echo "# github-actions-example" >> README.md
-//        git init
-//        git add README.md
-//        git commit -m "first commit"
-//        git branch -M main
-//        git remote add origin https://github.com/AliArtukov/github-actions-example.git
-//        git push -u origin main
